dfe04a2f4172a8325088dd2508bbe27b329293e1
Jeremy <jerimonski@gmail.com>
2025-08-11 16:56:17 -0400

    feat: new features

--- test1.py
+++ test1.py
@@ -1,24 +1,39 @@
 # Escribir un codigo en python para comprobar si una direccion
-# IP funciona o esta caida
-
-#hacer una web que haga un ping constante en vivo de los ip que tenga
-#se debe poder ver en tiempo real los dispositivos acticos y cuando
-# uno se caiga, debe manda un correo avisando de que se cayo
+# IP funciona o esta caida.
 
 # subprocess: Modulo para ejecutar comandos del sistema
 # platform: Se ocupa identificar si el codigo se esta ejecutando en Windows o Linux
-# requests: Modulo para hacer peticiones a una API
-#json: transforma la respuesta de la API a un json.
-
 import subprocess 
 import platform
-import requests
-import json
-import socket
-import ipaddress
 
-res = requests.get("https://689a0d64fed141b96ba1b4fb.mockapi.io/Ip_List/ip_list")
-response = json.loads(res.text)
+ip_list = {
+    "Local":"127.0.0.1",
+    "CORTAFUEGO": "10.30.10.1", 
+     "SW_PRINCIPAL": "10.30.10.2", 
+    "CORTAFUEGO respaldo": "10.30.10.3", 
+    "Informatica": "10.30.10.5", 
+    "Secretaria Municipal": "10.30.10.6", 
+    "Informatica_JP" :"10.30.10.7", 
+    "Sala SW_SERVER_MAHO": "10.30.10.8", 
+    "Informatica_Jorge": "10.30.10.9", 
+    "Servidores": "10.30.10.10", 
+    "Traspasado": "10.30.10.11", 
+    "Dom": "10.30.10.12", 
+    "Finanzas": "10.30.10.13", 
+    "Dideco 1": "10.30.10.14", 
+    "Transito": "10.30.10.15", 
+    "Educacion": "10.30.10.16", 
+    "DAO": "10.30.10.17", 
+    "Sala Concejo": "10.30.10.18", 
+    "Dideco 2": "10.30.10.19", 
+    "Secoplac": "10.30.10.20", 
+    "Juridico": "10.30.10.21", 
+    "Contabilidad": "10.30.10.22", 
+    "Licencia Conducir":"10.30.10.23", 
+    "DAO 2": "10.30.10.24", 
+    "Secoplac 2": "10.30.10.25", 
+    "Personal":"10.30.10.26",
+}
 
 ping_command = ["ping"]
 
@@ -31,11 +46,7 @@ if platform.system().lower() == "windows":
 else:
     ping_command.extend(["-c", "1"])
 
-for item in response:
-    name = item.get('name')
-    ip = item.get('ip')
-    network_mask = ipaddress.ip_network(f"{ip}/24", strict=False)
-    subnet_mask = network_mask.netmask
+for name, ip in ip_list.items():
     try:
         # se encarga de transformar el mensaje de respuesta 
         # a texto legible, donde captura la respuesta y el texto, 
@@ -45,18 +56,11 @@ for item in response:
             capture_output=True,
             text=True,
             timeout=1
-        )
+        ) 
         # siguiendo la convencion, cualquier respuesta igual a 0,
         # es una respuesta exitosa y cualquier otra es erronea
         if result.returncode == 0:
-            try:
-                # mediante la ip, busca y guarda el nombre 
-                # del dispositivo en caso de que tenga
-                hostname_info = socket.gethostbyaddr(ip)
-                hostname = hostname_info[0]
-                print(f"✅ {name} ({ip}) ({subnet_mask}): Host en línea. Nombre del sistema:✅ {hostname}.")
-            except socket.herror:
-                print(f"✅ {name} ({ip}) ({subnet_mask}): Host en línea. Nombre del sistema:❌ No disponible.")
+            print(f"✅ {name} ({ip}): Host esta en linea.")
         else:
             print(f"❌ {name} ({ip}): Host no responde.")
     except subprocess.TimeoutExpired:
